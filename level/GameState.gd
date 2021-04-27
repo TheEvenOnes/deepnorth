7bb9f26e84491e33dce5fd18c7d163614f40f312
dcerjan <domagoj.cerjan@gmail.com>
2021-04-27 02:02:28 +0200

    gh-pages setup

--- level/GameState.gd
+++
@@ -1,4 +0,0 @@
-extends Resource
-class_name GameState
-
-var day = 1
--- object/Torch.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-onready var player = $Light/AnimationPlayer
-
-func _ready() -> void:
-	var offset = randf() * 0.5
-	player.play('Idle')
-	player.seek(offset)
